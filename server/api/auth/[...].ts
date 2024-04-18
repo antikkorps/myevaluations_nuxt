3496cefdc1c534c991b2c2d8b8c92de4f743fd42
Franck <franckvienot7@gmail.com>
2024-04-18 17:18:49 +0200

    add authjs-nuxt and start configuring it

---
+++ server/api/auth/[...].ts
@@ -0,0 +1,99 @@
+// import GithubProvider from "@auth/core/providers/github"
+import credentialsProvider from "@auth/core/providers/credentials"
+import type { AuthConfig } from "@auth/core/types"
+import { NuxtAuthHandler } from "#auth"
+import bcrypt from "bcrypt"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+// The #auth virtual import comes from this module. You can use it on the client
+// and server side, however not every export is universal. For example do not
+// use sign-in and sign-out on the server side.
+
+const runtimeConfig = useRuntimeConfig()
+
+// Refer to Auth.js docs for more details
+export const authOptions: AuthConfig = {
+  secret: runtimeConfig.authJs.secret,
+  providers: [
+    credentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "email" },
+        password: { label: "password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        if (typeof credentials.email !== "string") {
+          throw new Error("Invalid email")
+        }
+        if (
+          typeof credentials.email !== "string" ||
+          typeof credentials.password !== "string"
+        ) {
+          throw new Error("Invalid credentials")
+        }
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        })
+
+        if (!user) {
+          throw new Error("No user found")
+        }
+
+        const isValid = await bcrypt.compare(credentials.password, user.password)
+
+        if (!isValid) {
+          throw new Error("Invalid password")
+        }
+        return {
+          id: user.id,
+          email: user.email,
+          firstName: user.firstname,
+          lastName: user.lastname,
+          avatar: user.avatar,
+          role: user.role,
+        }
+      },
+    }),
+    // GithubProvider({
+    //   clientId: runtimeConfig.github.clientId,
+    //   clientSecret: runtimeConfig.github.clientSecret
+    // }),
+  ],
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.id = user.id
+        token.email = user.email
+        token.firstName = user.firstname
+        token.lastName = user.lastname
+        token.avatar = user.avatar
+        token.role = user.role
+      }
+      return token
+    },
+    session: async ({ session, token }) => {
+      session.user = session.user || {
+        id: "",
+        firstName: "",
+        lastName: "",
+        avatar: "",
+        role: "",
+      }
+      session.user.id = token.id as string
+      session.user.email = token.email as string
+      session.user.firstName = token.firstName as string
+      session.user.lastName = token.lastName as string
+      session.user.avatar = token.avatar as string
+      session.user.role = token.role as string
+      return session
+    },
+  },
+}
+
+export default NuxtAuthHandler(authOptions, runtimeConfig)
+// If you don't want to pass the full runtime config,
+//  you can pass something like this: { public: { authJs: { baseUrl: "" } } }
