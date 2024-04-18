7cf8de85332d58f7f49608ecea21db1bad17b4c1
Franck <franckvienot7@gmail.com>
2024-04-18 23:07:43 +0200

    almost done with configuring authJs

--- pages/test.vue
+++ pages/test.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
-const { signIn, signOut, session, status, cookies, getProviders } = useAuth()
+const { signIn, signOut, session, status, user, cookies, getProviders } = useAuth()
 </script>
 
 <template>
   <div>
-    <div>
+    <div class="flex flex-col">
       <a href="/api/auth/signin" class="buttonPrimary">Native Link Sign in</a>
-      <button @click="signIn(`github`)">JS Sign In</button>
-      <button @click="signOut()">Sign Out</button>
+      <button @click="signIn()">JS Sign In</button>
+      <button class="primary" @click="signOut()">Sign Out</button>
     </div>
     <div>
-      <pre>{{ status }}</pre>
-      <pre>{{ session?.user }}</pre>
+      <pre>status : {{ status }}</pre>
+      <pre>session user : {{ session?.user }}</pre>
       <pre>cookie : {{ cookies }}</pre>
+      <pre>user :  {{ session?.user.firstname }}</pre>
+      <pre>role : {{ session?.user.role }}</pre>
     </div>
   </div>
 </template>
