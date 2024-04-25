503378dcceb8d19e84ad29c46e9a2961885d5f4a
Franck <franckvienot7@gmail.com>
2024-04-25 23:33:44 +0200

    add select to popover in order to have a better user experience

---
+++ components/ui/AddField.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const props = defineProps({
+  options: {
+    type: Array,
+    required: true,
+  },
+  addField: {
+    type: Function,
+    required: true,
+  },
+})
+
+const fieldOptions = ["texte", "nombre", "date", "heure", "liste"]
+
+const fieldOption = ref(fieldOptions[0])
+</script>
+
+<template>
+  <div>
+    <UPopover overlay>
+      <UButton color="white" label="+" />
+      <template #panel="{ close }">
+        <div class="p-8">
+          <USelect
+            v-model="fieldOption"
+            :options="fieldOptions"
+            @click="addField(fieldOption)"
+          />
+          <UButton label="Retour" @click="close" class="my-2" />
+        </div>
+      </template>
+    </UPopover>
+  </div>
+</template>
--- components/ui/AddInput.vue
+++ components/ui/AddInput.vue
@@ -10,23 +10,37 @@ const props = defineProps({
     required: true,
   },
 })
+
+function addField(option) {
+  console.log(option)
+}
 </script>
 <template>
-  <div class="flex flex-row items-center space-x-2 py-2">
-    <UFormGroup label="Entrez le titre du champ">
-      <UInput
-        variant="none"
-        icon="i-heroicons-pencil"
-        placeholder="Entrez le titre du champ"
-        v-model="field.label"
-      />
-    </UFormGroup>
+  <div>
+    <div class="flex flex-row items-center space-x-2 py-2">
+      <UFormGroup label="Entrez le titre du champ">
+        <UInput
+          variant="none"
+          icon="i-heroicons-pencil"
+          placeholder="Entrez le titre du champ"
+          v-model="field.label"
+        />
+      </UFormGroup>
 
-    <UFormGroup :label="field.label">
-      <div class="flex flex-row space-x-2">
-        <UInput :type="inputType" v-model="field.value" :name="field.name" />
-        <UiDeleteFieldButton :removeField="removeField" />
-      </div>
-    </UFormGroup>
+      <UFormGroup :label="field.label">
+        <div class="flex flex-row space-x-2">
+          <UInput
+            :type="inputType"
+            v-model="field.value"
+            :name="field.name"
+            :placeholder="inputType"
+          />
+          <UiDeleteFieldButton :removeField="removeField" />
+        </div>
+      </UFormGroup>
+    </div>
+    <div class="flex justify-center items-center mx-auto">
+      <UiAddField :options="fieldOptions" :add-field="addField" />
+    </div>
   </div>
 </template>
